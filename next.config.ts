ced4fe248ff8b51a7e869c034f9ac9ead1e84d0d
lalamoVs <jasonwong22015@gmail.com>
2026-08-22 10:03:03 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-};
-// next.config.js
-module.exports = {
-  // ... other config
   outputFileTracingIncludes: {
-    '/*': ['node_modules/@swc/helpers/**/*']
-  }
-}
+    "/*": ["node_modules/@swc/helpers/**/*"],
+  },
+};
 
 export default nextConfig;
